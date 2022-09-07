48e7e4f612c616d35dbddd5c18b3db1f941699ad
Andrew Maksimenko <a.maksimenko@dunice.net>
2022-09-07 16:17:48 +0300

    finished Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
 
-import './App.css';
+import styles from './App.module.css';
 
 import { Home } from './pages/Home';
 import { Pin } from './pages/Pin';
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path='/pin' element={<Pin />} />
-        <Route path='*' element={<Navigate to='/pin' replace />} />
-      </Routes>
-    </BrowserRouter>
+    <main className={styles.appBackground}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/pin' element={<Pin />} />
+          <Route path='*' element={<Navigate to='/pin' replace />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import style from './Home.module.css';
+
+export function Home() {
+  return (
+    <section className={style.wrapper}>
+      <p className={style.title}>Home</p>
+    </section>
+  );
+}
